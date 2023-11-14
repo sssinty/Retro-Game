a4ce9dc386277518ccd492ce653146bf79a42f59
KirillLaptop <sinty.kpov@gmail.com>
2023-11-14 13:10:52 +0300

    deleted src folder

--- src/js/__test__/Character.test/Daemon.test.js
+++
@@ -1,7 +0,0 @@
-import Daemon from '../../character/Daemon';
-
-test('properly created object', () => {
-  const bowman = new Daemon(1);
-  const correct = {attack: 10, defence: 10, health: 50, level: 1, type: 'daemon'}
-  expect(bowman).toEqual(correct);
-})
